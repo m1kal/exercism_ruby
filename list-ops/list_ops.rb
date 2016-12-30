68166a2579a119079bb8ee2f11cdbe1c78b4042d
mikal <mikal82@protonmail.com>
2016-12-30 22:16:51 +0100

    Solved but ugly

--- list-ops/list_ops.rb
+++ list-ops/list_ops.rb
@@ -23,9 +23,57 @@ class ListOps
     array
   end
 
-  def self.concatter(input)
+  def self.concatter(input1, input2)
+    append(append([], input1), input2)
+  end
+
+  def self.append(array, input)
+    size = arrays(input)
+    0.upto(size - 1) do |idx|
+      array << input[idx]
+    end
+    array
   end
 
   def self.mapper(input, &block)
+    return input unless block_given?
+    array = []
+    size = arrays(input)
+    0.upto(size - 1) do |idx|
+      array << yield(input[idx])
+    end
+    array
   end
+
+  def self.filterer(input, &block)
+    return input unless block_given?
+    array = []
+    size = arrays(input)
+    0.upto(size - 1) do |idx|
+      array << input[idx] if yield(input[idx])
+    end
+    array
+  end
+
+  def self.sum_reducer(input)
+    value = 0
+    size = arrays(input)
+    0.upto(size - 1) do |idx|
+      value += input[idx]
+    end
+    value
+  end
+
+  def self.factorial_reducer(input)
+    value = 1
+    size = arrays(input)
+    0.upto(size - 1) do |idx|
+      value *= input[idx]
+    end
+    value
+  end
+end
+
+module BookKeeping
+  VERSION = 2
 end
