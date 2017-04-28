3ac90d6f0b84927acfa77ae35bfdc01ddee1e9c2
mikal <mikal82@protonmail.com>
2017-04-28 12:37:05 +0200

    OCR numbers - V2

--- ocr-numbers/ocr_numbers.rb
+++ ocr-numbers/ocr_numbers.rb
@@ -1,16 +1,16 @@
 class OcrNumbers
-  DIGITS = [
-    [[' _ '], ['| |'], ['|_|'], ['   ']].join,
-    [['   '], ['  |'], ['  |'], ['   ']].join,
-    [[' _ '], [' _|'], ['|_ '], ['   ']].join,
-    [[' _ '], [' _|'], [' _|'], ['   ']].join,
-    [['   '], ['|_|'], ['  |'], ['   ']].join,
-    [[' _ '], ['|_ '], [' _|'], ['   ']].join,
-    [[' _ '], ['|_ '], ['|_|'], ['   ']].join,
-    [[' _ '], ['  |'], ['  |'], ['   ']].join,
-    [[' _ '], ['|_|'], ['|_|'], ['   ']].join,
-    [[' _ '], ['|_|'], [' _|'], ['   ']].join
-  ].freeze
+  DIGITS = {
+    [[' _ '], ['| |'], ['|_|'], ['   ']].join => '0',
+    [['   '], ['  |'], ['  |'], ['   ']].join => '1',
+    [[' _ '], [' _|'], ['|_ '], ['   ']].join => '2',
+    [[' _ '], [' _|'], [' _|'], ['   ']].join => '3',
+    [['   '], ['|_|'], ['  |'], ['   ']].join => '4',
+    [[' _ '], ['|_ '], [' _|'], ['   ']].join => '5',
+    [[' _ '], ['|_ '], ['|_|'], ['   ']].join => '6',
+    [[' _ '], ['  |'], ['  |'], ['   ']].join => '7',
+    [[' _ '], ['|_|'], ['|_|'], ['   ']].join => '8',
+    [[' _ '], ['|_|'], [' _|'], ['   ']].join => '9'
+  }.freeze
 
   def self.convert(input)
     find_digit_groups(input).collect { |group| convert_group(group) }.join(',')
@@ -22,7 +22,7 @@ class OcrNumbers
 
   def self.convert_digit(digit)
     raise ArgumentError unless digit.length == 12
-    (DIGITS.index(digit) || '?').to_s
+    DIGITS[digit] || '?'
   end
 
   def self.find_digit_groups(input)
