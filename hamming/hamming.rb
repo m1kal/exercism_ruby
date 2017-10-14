7663ce63c7a2ce46b8266b157937fc2e2669fb78
mikal <mikal82@protonmail.com>
2017-10-14 20:10:12 +0200

    Hamming v4

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,14 +1,8 @@
 # a
 class Hamming
-  # strictly speaking not a copy, but influenced by others' solutions
-  # and suggestions from @remcopeereboom
   def self.compute(str1, str2)
-    if str1.size != str2.size
-      raise ArgumentError, 'Input strands need to have the same length'
-    end
-    str1.size.times.count do |idx|
-      str1[idx] != str2[idx]
-    end
+    raise ArgumentError, 'Length mismatch' if str1.size != str2.size
+    str1.split('').zip(str2.split('')).map(&:uniq).flatten.size - str1.size
   end
 end
 
